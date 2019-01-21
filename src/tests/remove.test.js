aeddaf6c124935a66a5b575e7ebc3f91a329ba15
Bodyan322 <bogdan3206@gmail.com>
2019-01-21 15:06:18 +0200

    add changes to 1 test

--- src/tests/remove.test.js
+++ src/tests/remove.test.js
@@ -5,12 +5,14 @@ describe('tests for method remove', () => {
     const hashTable = new HashTable(5);
     expect(hashTable.remove).toBeInstanceOf(Function);
   });
-  test('should track actual keys', () => {
+  test('should delete elements correctly', () => {
     const hashTable = new HashTable(1);
 
     hashTable.add('a', 'sky-old');
+    hashTable.add('b', 'sun');
     hashTable.remove('a');
 
     expect(hashTable.table[0]).toBeUndefined();
+    expect(hashTable.table[1]).toEqual({ key: 'b', value: 'sun' });
   });
 });
